43068264f130fd94d8992bfa9cf63ecc503ff2af
8rabin <48649348+8rabin@users.noreply.github.com>
2019-03-19 22:43:43 +0100

    Delete RebuildPage.sh

--- RebuildPage.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -u yourname:4a55a3bbc85da8e3c87141ff3229574bee9663ac -X POST https://api.github.com/repos/8rabin/jbwa/pages/builds -H "Accept: application/vnd.github.mister-fantastic-preview+json"
